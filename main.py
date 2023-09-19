3d1a56600bfac9a4e86f5d4411f3abddcc9c8693
HanjoPurebuddha <hanjoyoutaku@gmail.com>
2023-09-19 16:54:44 +0100

    Added entities

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-import os
-from rpsrunner import run_tournament
+import argparse
+import tournament
+
+# COMMENTER ENTITY:
+# inquiry: What is the main purpose of this module?
+# answer: This module serves as the command-line interface (CLI) for running the Rock-Paper-Scissors tournament.
 
 def main():
-    """
-    Main function to run the RPS Tournament.
-    """
-    os.system('clear')
-    print("Welcome to the RPS Tournament!")
+    # Set up argument parser
+    parser = argparse.ArgumentParser(description="Run a Rock-Paper-Scissors tournament.")
     
+    # Add arguments
+    parser.add_argument("--rounds", type=int, default=1000, help="Number of rounds in the tournament.")
+    args = parser.parse_args()
+
+    # PRINT CREATOR ENTITY:
+    print(f"Starting the Rock-Paper-Scissors tournament with {args.rounds} rounds!")
+
     # Run the tournament
-    run_tournament()
+    tournament.run_tournament(num_rounds=args.rounds)
 
 if __name__ == "__main__":
     main()
