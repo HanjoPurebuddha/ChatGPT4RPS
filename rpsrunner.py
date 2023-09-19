be59522ad041f528ff476fe3ec9e60c1643a2b41
HanjoPurebuddha <hanjoyoutaku@gmail.com>
2023-09-19 16:54:07 +0100

    regenerated with http://sophiaintelligence.ai research

--- rpsrunner.py
+++ rpsrunner.py
@@ -1,61 +1,46 @@
-
-import os
-import sys
 import random
-import multiprocessing
+from bestrps import bestrps_play
+from LLMagent import llm_play
+from meta_rps_agent import output as meta_rps_play
 
-# Function to load a bot from a specified file
-def load_bot(filename):
-    with open(filename, 'r') as file:
-        code = compile(file.read(), filename, 'exec')
-    bot_namespace = {}
-    exec(code, bot_namespace)
-    return bot_namespace['move']
-
-# Function to play a match between two bots
-def play_match(bot_a, bot_b, num_games=1000):
-    history = {"A": [], "B": []}
-    score = {"A": 0, "B": 0, "Tie": 0}
-    for _ in range(num_games):
-        move_a = bot_a(history["B"], history["A"])
-        move_b = bot_b(history["A"], history["B"])
-        history["A"].append(move_a)
-        history["B"].append(move_b)
-
-        if move_a == move_b:
-            score["Tie"] += 1
-        elif (move_a == "R" and move_b == "S") or (move_a == "P" and move_b == "R") or (move_a == "S" and move_b == "P"):
-            score["A"] += 1
+def run_game(player1, player2, num_rounds=1000):
+    """
+    Run the RPS game between two players for a certain number of rounds.
+    Returns the results of the game.
+    """
+    score = {"player1": 0, "player2": 0, "tie": 0}
+    history = {"player1": [], "player2": []}
+    
+    for _ in range(num_rounds):
+        move1 = player1(history)
+        move2 = player2(history)
+        
+        history["player1"].append(move1)
+        history["player2"].append(move2)
+        
+        if move1 == move2:
+            score["tie"] += 1
+        elif (move1 == "R" and move2 == "S") or (move1 == "S" and move2 == "P") or (move1 == "P" and move2 == "R"):
+            score["player1"] += 1
         else:
-            score["B"] += 1
+            score["player2"] += 1
+    
     return score
 
-# Function to run a series of matches in parallel
-def run_matches(bots, num_games=1000, num_processes=4):
-    pool = multiprocessing.Pool(processes=num_processes)
-    results = []
-
-    for i, bot_a in enumerate(bots):
-        for j, bot_b in enumerate(bots):
-            if i < j:
-                result = pool.apply_async(play_match, (bot_a, bot_b, num_games))
-                results.append(result)
+def run_tournament():
+    """
+    Run the RPS tournament between multiple players.
+    """
+    players = [bestrps_play, llm_play, meta_rps_play]
+    results = {}
     
-    pool.close()
-    pool.join()
-
-    for result in results:
-        print(result.get())
-
-# Main function to initiate the bot matches
-def main():
-    if len(sys.argv) < 3:
-        print(f"Usage: {sys.argv[0]} <bot_file_1> <bot_file_2> [<bot_file_3> ...]")
-        sys.exit(1)
+    for i, player1 in enumerate(players):
+        for j, player2 in enumerate(players):
+            if i != j:
+                result = run_game(player1, player2)
+                results[f"player{i+1}_vs_player{j+1}"] = result
     
-    bot_files = sys.argv[1:]
-    bots = [load_bot(bot_file) for bot_file in bot_files]
-    run_matches(bots)
+    for match, result in results.items():
+        print(f"\nResults for {match}:")
+        print(f"Player1: {result['player1']}, Player2: {result['player2']}, Ties: {result['tie']}")
 
-if __name__ == "__main__":
-    main()
